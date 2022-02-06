146062b5231547480f58f86c93bde35d1616846e
Joe Dwonczyk <JDwonczyk@gmail.com>
2022-02-06 11:48:09 +0000

    Add Dashed Line in PathExample component

--- lib/ui/components/path_example.dart
+++ lib/ui/components/path_example.dart
@@ -42,6 +42,10 @@ class PathExample extends StatelessWidget {
     return LinePainter();
   }
 
+  CustomPainter _drawDashedLine(BuildContext context) {
+    return DashedLinePainter();
+  }
+
   CustomPainter _drawCubic(BuildContext context) {
     return CubicPainter();
   }
@@ -50,7 +54,7 @@ class PathExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: CustomPaint(
-        painter: _drawCubic(context),
+        painter: _drawDashedLine(context),
       ),
     );
   }
@@ -80,6 +84,50 @@ class LinePainter extends CustomPainter {
     return oldDelegate.progress != progress;
   }
 }
+
+class DashedLinePainter extends CustomPainter {
+  final double progress; // How far to draw line accross width of screen.
+
+  DashedLinePainter({this.progress = 0.5});
+
+  Paint _paint = Paint()
+    ..color = Colors.black
+    ..strokeWidth = 4.0
+    ..style = PaintingStyle.stroke
+    ..strokeJoin = StrokeJoin.round;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var path = Path()
+      ..moveTo(0, size.height / 2)
+      ..lineTo(size.width * progress, size.height / 2);
+
+    Path dashPath = Path();
+
+    double dashWidth = 10.0;
+    double dashSpace = 5.0;
+    double distance = 0.0;
+
+    for (var pathMetric in path.computeMetrics()) {
+      while (distance < pathMetric.length) {
+        dashPath.addPath(
+          pathMetric.extractPath(distance, distance + dashWidth),
+          Offset.zero,
+        );
+        distance += dashWidth;
+        distance += dashSpace;
+      }
+    }
+    canvas.drawPath(dashPath, _paint);
+  }
+
+  @override
+  bool shouldRepaint(DashedLinePainter oldDelegate) {
+    return oldDelegate.progress != progress;
+  }
+}
+
+
 
 class CubicPainter extends CustomPainter {
   CubicPainter();
