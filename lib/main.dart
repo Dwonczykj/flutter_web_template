54ba98b136dea2a6787755432199c2c0acfd165b
Joe Dwonczyk <JDwonczyk@gmail.com>
2022-02-08 18:16:06 +0000

    Data Hydration: Configure App to accept Data Providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'ui/screens/screens.dart';
+import 'ui_stage2/network/mock_service.dart';
+import 'ui_stage2/network/service_interface.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,21 +15,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Web Starting Kit',
-      theme: ThemeData(
-        // This is the theme of your application.§
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.orange,
+    return MultiProvider(
+      providers: [
+        //Services
+        Provider<ServiceInterface>(
+          create: (_) => MockService.create(),
+          lazy: false,
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Web Starting Kit',
+        theme: ThemeData(
+          // This is the theme of your application.§
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.orange,
+        ),
+        home: const MyHomePage(title: 'SVG Example'),
       ),
-      home: const MyHomePage(title: 'SVG Example'),
     );
   }
 }
